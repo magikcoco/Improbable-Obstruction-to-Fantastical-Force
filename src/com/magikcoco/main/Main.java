04c48f8dd55bb73ffc5a9e3f5693d42cf922f3ae
homepc <punishedtomato@gmail.com>
2023-02-20 18:56:21 -0500

    Added a TextArea and a TextField to the gui, changed the root node to an anchor pane, added an event handler to the textField, explicitly set that the application must termiante on close request to avoid calling launch() more than once through the shell

--- src/com/magikcoco/main/Main.java
+++ src/com/magikcoco/main/Main.java
@@ -3,6 +3,11 @@ package com.magikcoco.main;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -11,14 +16,48 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception{
-        int height = 480;
-        int width = 640;
-        Group root = new Group();
+        double height = 480.0;
+        double width = 640.0;
+
+        TextArea textArea = new TextArea();
+        textArea.setPrefHeight(height-40);
+        textArea.setPrefWidth(width-15);
+        textArea.setLayoutY(7.5);
+        textArea.setLayoutX(7.5);
+        textArea.setDisable(true);
+
+        TextField textField = new TextField();
+        textField.setPrefHeight(25);
+        textField.setPrefWidth(width-15);
+        textField.setLayoutY(textArea.getPrefHeight()+10);
+        textField.setLayoutX(7.5);
+        textField.setOnKeyPressed(event -> {
+            handleGuiKeyPress(event, textField, textArea);
+        });
+
+        AnchorPane root = new AnchorPane();
+        root.getChildren().add(textArea);
+        root.getChildren().add(textField);
+
         Scene scene = new Scene(root, width, height);
+
         stage.setScene(scene);
         stage.setTitle("Improbable Obstruction to Fantastical Force");
         stage.setResizable(false);
+        stage.setOnCloseRequest(event -> {
+            System.out.println("Closing application...");
+            System.exit(0);
+        });
+
         stage.show();
+    }
+
+    private void handleGuiKeyPress(KeyEvent event, TextField textField, TextArea textArea) {
+        if(event.getCode() == KeyCode.ENTER){
+            textArea.clear();
+            textArea.setText(textField.getText());
+            textField.clear();
+        }
     }
 
 
